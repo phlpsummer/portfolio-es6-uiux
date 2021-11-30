f12a1b78d047616460e55f336ce4ceb49c182ec3
phplpsummer <sh052100@naver.com>
2021-11-30 21:40:09 +0900

    isotope레이아웃 적용

--- js/myFlickr.js
+++ js/myFlickr.js
@@ -8,7 +8,7 @@ flickr.people.getPhotos
 
 
 */
-
+const loading = document.querySelector(".loading");
 const frame = document.querySelector(".list");
 const urlBase = "https://www.flickr.com/services/rest/?";
 const method1 = "flickr.people.getPhotos";
@@ -42,10 +42,37 @@ fetch(url1)
                         <p>${data.title}</p>
                         <span>${data.owner}</span>
                     </div>
-                    <a href="${imgSrc}"><img src="${thumbSrc}"></a>
+                    <a href="${imgSrc}"><img src="${thumbSrc}" class="thumb"></a>
                 </div>
             </li>
         `;
     });
     frame.innerHTML = htmls;
+
+    const imgs = frame.querySelectorAll("img");
+    const lenImg = imgs.length;
+    let countImg = 0;
+    for(let el of imgs){
+        el.addEventListener("load",()=>{
+            countImg++;
+
+            if(countImg == lenImg) isotopeLayout();
+        });
+
+        let thumbImg = el.closest(".item").querySelector(".thumb");
+        thumbImg.addEventListener("error",(e)=>{
+            e.currentTarget.closest(".item").querySelector(".thumb").setAttribute("src","img/default.jpg");
+        });
+    }
 })
+
+function isotopeLayout(){
+    loading.classList.add("off");
+    frame.classList.add("on");
+
+    new Isotope(".list",{
+        itemSelector: ".item",
+        columnWidth: ".item",
+        transitionDuration: "0.5s"
+    });
+}
